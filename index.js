f706cc92a72ae925ca029dd1640b65efa0b0f129
Alex Blackburn <Alex>
2022-04-30 23:40:07 -0400

    commit tree ced11b0063513e8857c33e01412d74f85ca777bd

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block > 42) {
+        return block - 42
+    } else return 42 - block
+};
+
+distanceFromHqInBlocks(block);
+
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264;
+};
+
+
+function distanceTravelledInFeet(start, destination) {
+    if (start < destination) {
+        return (destination - start) * 264
+    } else {
+        return (start - destination) * 264;
+    }
+};
+
+function calculatesFarePrice(start, destination) {
+    const travel = distanceTravelledInFeet(start, destination);
+
+    if (travel <= 400) {
+        return 0;
+    } else if (travel > 400 && travel <= 2000) {
+        return .02 * (travel - 400);
+    } else if (travel > 2000 && travel < 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
